c1847224962905755124c46234f9c94b21b2a320
chengzhia <ruanchengzhia@outlook.com>
2017-06-06 14:45:03 +0800

    springcloud

---
+++ spring-cloud-ribbon/src/main/java/com/chengzhi/web/entity/User.java
@@ -0,0 +1,32 @@
+package com.chengzhi.web.entity;
+
+public class User {
+	private String userId;
+	private String userName;
+	private String userPassword;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+}
